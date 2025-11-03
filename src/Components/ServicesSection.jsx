c9506c9ccd3a0014beda68278039246c17a7cfae
Pankaj Barakoti <pankajbarakoti837@gmail.com>
2025-11-03 16:03:04 +0530

    raiso

--- src/Components/ServicesSection.jsx
+++ src/Components/ServicesSection.jsx
@@ -23,7 +23,7 @@ const services = [
 
 const ServicesSection = () => {
   return (
-    <section className="bg-white md:py-10 px-4 sm:px-6 lg:px-8 font-sans">
+    <section className="bg-white md:py-10 px-4 sm:px-6 py-10 lg:px-8 font-sans">
       <div className="max-w-7xl mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {services.map((service, index) => (
